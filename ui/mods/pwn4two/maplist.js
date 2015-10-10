382897c96626cbc1881b948aa5c3dd3e4038278e
Alpha2546 <adriaanvanloon@hotmail.com>
2015-10-10 22:39:56 +0200

    added neccesary files

---
+++ ui/mods/pwn4two/maplist.js
@@ -0,0 +1,6 @@
+﻿cShareSystems.load_pas("pwnt4two", [
+	"coui://ui/mods/pwn4two/systems/splitter.pas",	
+	"coui://ui/mods/alpha2546/systems/the_island.pas",	
+	"coui://ui/mods/alpha2546/systems/geyser.pas",
+	"coui://ui/mods/alpha2546/systems/jungle_hazard.pas"		
+]);
