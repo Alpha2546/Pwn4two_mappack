ace5e20d03add049136a7fdb57fc2cc05a61a75f
Pwn4two <henry_wiltcher@hotmail.com>
2015-10-12 18:38:16 +0100

    Added a map, "control".

--- ui/mods/pwn4two/maplist.js
+++ ui/mods/pwn4two/maplist.js
@@ -3,6 +3,7 @@ cShareSystems.load_pas("pwnt4two", [
 	"coui://ui/mods/pwn4two/systems/4v4.pas",	
 	"coui://ui/mods/pwn4two/systems/kappa_shared.pas",	
 	"coui://ui/mods/pwn4two/systems/5v5_shared.pas",
+        "coui://ui/mods/pwn4two/systems/control.pas",
 	"coui://ui/mods/pwn4two/systems/crackdown.pas",
 	"coui://ui/mods/pwn4two/systems/6_player.pas"
 ]);
