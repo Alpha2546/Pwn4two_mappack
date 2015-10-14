81afdcc82621aae1119997c88f309009adc59bf1
Pwn4two <henry_wiltcher@hotmail.com>
2015-10-15 00:58:35 +0100

    Updated maplist.js

--- ui/mods/pwn4two/maplist.js
+++ ui/mods/pwn4two/maplist.js
@@ -1,4 +1,4 @@
-cShareSystems.load_pas("pwnt4two", [
+cShareSystems.load_pas("pwn4two", [
 	"coui://ui/mods/pwn4two/systems/1v1_or_shared.pas",	
 	"coui://ui/mods/pwn4two/systems/4v4.pas",	
 	"coui://ui/mods/pwn4two/systems/kappa_shared.pas",	
