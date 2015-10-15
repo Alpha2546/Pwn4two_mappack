7e70b6999162697fb6af4501fd6bdc1ccc71d730
Pwn4two <henry_wiltcher@hotmail.com>
2015-10-15 01:08:23 +0100

    Took out farenheit and control

--- ui/mods/pwn4two/maplist.js
+++ ui/mods/pwn4two/maplist.js
@@ -3,8 +3,8 @@ cShareSystems.load_pas("pwn4two", [
 	"coui://ui/mods/pwn4two/systems/4v4.pas",	
 	"coui://ui/mods/pwn4two/systems/kappa_shared.pas",	
 	"coui://ui/mods/pwn4two/systems/5v5_shared.pas",
-        "coui://ui/mods/pwn4two/systems/control.pas",
+        
 	"coui://ui/mods/pwn4two/systems/crackdown.pas",
-        "coui://ui/mods/pwn4two/systems/farenheit.pas"
+        
 	"coui://ui/mods/pwn4two/systems/6_player.pas"
 ]);
