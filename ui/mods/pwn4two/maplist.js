2595762aad8b297fccb1cb8adb7d4f09f7a1ac74
Pwn4two <henry_wiltcher@hotmail.com>
2015-10-15 19:35:28 +0100

    Added Mania

--- ui/mods/pwn4two/maplist.js
+++ ui/mods/pwn4two/maplist.js
@@ -1,4 +1,4 @@
-cShareSystems.load_pas("pwn4two", [
+cShareSystems.load_pas("Pwn4two", [
 	"coui://ui/mods/pwn4two/systems/1v1_or_shared.pas",	
 	"coui://ui/mods/pwn4two/systems/4v4.pas",	
 	"coui://ui/mods/pwn4two/systems/kappa_shared.pas",	
@@ -7,4 +7,5 @@ cShareSystems.load_pas("pwn4two", [
 	"coui://ui/mods/pwn4two/systems/crackdown.pas",
         "coui://ui/mods/pwn4two/systems/farenheit.pas",
 	"coui://ui/mods/pwn4two/systems/6_player.pas"
+        "coui://ui/mods/pwn4two/systems/mania.pas"
 ]);
