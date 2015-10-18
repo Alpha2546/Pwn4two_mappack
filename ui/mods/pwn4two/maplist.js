3b3a869481fe0b8245399e7430f30a9f797314b0
Pwn4two <henry_wiltcher@hotmail.com>
2015-10-18 17:29:39 +0100

    Fixed maplist

--- ui/mods/pwn4two/maplist.js
+++ ui/mods/pwn4two/maplist.js
@@ -4,10 +4,10 @@ cShareSystems.load_pas("Pwn4two", [
         "coui://ui/mods/pwn4two/systems/control.pas",
         "coui://ui/mods/pwn4two/systems/farenheit.pas",
         "coui://ui/mods/pwn4two/systems/crackdown.pas",
-        "coui://ui/mods/pwn4two/systems/mania.pas"
+        "coui://ui/mods/pwn4two/systems/mania.pas",
         "coui://ui/mods/pwn4two/systems/6_player.pas",	
 	"coui://ui/mods/pwn4two/systems/4v4.pas",	
         "coui://ui/mods/pwn4two/systems/chronos.pas",	
 	"coui://ui/mods/pwn4two/systems/5v5_shared.pas",
-        "coui://ui/mods/pwn4two/systems/all_out_war.pas",
+        "coui://ui/mods/pwn4two/systems/all_out_war.pas"
 ]);
