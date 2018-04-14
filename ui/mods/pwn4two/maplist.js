da7cf8f46514f63b016d7df4a45cef553c717899
Pwn4two <henry_wiltcher@hotmail.com>
2018-04-14 13:54:49 +0100

    Added more maps
    Added; Duel planets, Dualdecahedron and Dog Eat Dog.

--- ui/mods/pwn4two/maplist.js
+++ ui/mods/pwn4two/maplist.js
@@ -29,6 +29,10 @@ cShareSystems.load_pas("Pwn4two", [
 	"coui://ui/mods/pwn4two/systems/wadiya_2018.pas",
         "coui://ui/mods/pwn4two/systems/all_out war.pas",
 	"coui://ui/mods/pwn4two/systems/war_of the worlds.pas",
+	"coui://ui/mods/pwn4two/systems/duel_planets",
 	"coui://ui/mods/pwn4two/systems/the_big squeeze.pas",
-	"coui://ui/mods/pwn4two/systems/32_friends.pas"
+	"coui://ui/mods/pwn4two/systems/32_friends.pas",
+	"coui://ui/mods/pwn4two/systems/dualdecahedron"
+	"coui://ui/mods/pwn4two/systems/dog_eat dog.pas"
+
 ]);
